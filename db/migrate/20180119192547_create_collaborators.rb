bf6310ccb5653f3e703a22e11699779b76876627
Tucker Platt <tucker@bendcable.com>
2018-01-19 23:15:29 +0000

    current build for slack

--- db/migrate/20180119192547_create_collaborators.rb
+++ db/migrate/20180119192547_create_collaborators.rb
@@ -1,18 +1,8 @@
 class CreateCollaborators < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
-      t.string :user_id
-      t.timestamps
-    end
-    
-    create_table  :wikis do |t|
-      t.string :id
-      t.timestamps
-    end
-  
-    create_table :collaborators, id: false do |t|
-      t.integer :user_id
-      t.integer :id
+    create_table :collaborators do |t|
+      t.belongs_to :user
+      t.belongs_to :id
       t.timestamps
     end
   end  
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104221754) do
+ActiveRecord::Schema.define(version: 20180119192547) do
+
+  create_table "collaborators", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
